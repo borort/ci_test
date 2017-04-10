377a5769ac023782f348663396359b394ec250dc
borort <borort@gmail.com>
2017-04-10 17:57:43 +0900

    updated deployment script and index.html

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -3,8 +3,8 @@
 ssh -v -i ~/.ssh/id_staging ubuntu@54.238.233.243 << EOF
 
 echo '1. Updating sources'
-cd ~/GitHub/
-git checkout --force master
+cd ~/GitHub/ci_test/
+#git checkout --force master
 git pull
 
 #echo "2. Restart apache"
